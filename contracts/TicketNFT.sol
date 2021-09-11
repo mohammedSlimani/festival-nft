e013ffa317bb2f78a85d701a76e8ff486fa00ad4
mohammed <slimani.med.21@gmail.com>
2021-09-11 21:19:40 +0200

    selling feature implemented

--- contracts/TicketNFT.sol
+++ contracts/TicketNFT.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -10,14 +11,17 @@ contract TicketNFT is ERC721 {
 
     struct Ticket {
         uint256 price;
+        uint256 id; // for querying purposes
+        address owner;
     }
 
     mapping(uint256 => Ticket) public tickets; // ticketId to ticket
+    mapping(address => uint256[]) public accountToTickets;
     uint256 public totalTickets;
     uint256 public initTicketPrice;
     address private organizer;
 
-    constructor (address _organizer, uint256 _initTicketPrice, uint256 _totalTickets) public ERC721 ("FestivalNFT", "FST") {
+    constructor (address _organizer, uint256 _initTicketPrice, uint256 _totalTickets) ERC721 ("FestivalNFT", "FST") {
         organizer = _organizer;
         initTicketPrice = _initTicketPrice;
         totalTickets = _totalTickets;
@@ -43,10 +47,55 @@ contract TicketNFT is ERC721 {
         for (uint i = 0; i < _numberOfTicket; i++) {
             ticketIds.increment();
             uint256 ticketId = ticketIds.current();
-            _mint(_recipient, ticketId);
+            _safeMint(_recipient, ticketId);
             tickets[ticketId] = Ticket({
-            price: initTicketPrice
+                price: initTicketPrice,
+                id: ticketId,
+                owner: _recipient
             });
+            accountToTickets[_recipient].push(ticketId);
         }
     }
+
+
+    function canSellTicketForPrice (uint256 _ticketId, uint256 _sellingPrice) public view returns (bool) {
+        require(tickets[_ticketId].price != 0, 'Ticket id not found');
+        return _sellingPrice < (tickets[_ticketId].price * 110) / 100;
+    }
+
+    function sellTicket (address _from, address _to, uint256 _ticketId, uint256 _sellingPrice) public {
+        require(_from == msg.sender, "Only owner of ticket can sell");
+        require(canSellTicketForPrice(_ticketId, _sellingPrice), "Price is higher than 110% of previous sale");
+        require(_to != organizer, "Can't sell to the organizer");
+        _transfer(_from, _to, _ticketId);
+        tickets[_ticketId].price = _sellingPrice;
+        tickets[_ticketId].owner = _to;
+        accountToTickets[_to].push(_ticketId);
+        // We dont remove the ticketId from the accountToTickets[_from] because it is very expensive
+        // Querying shouldn't be done in the blockchain in the first place
+        // Implementing a server that can act as a cache is the ideal solution
+    }
+
+    function getTicketsOfAccount (address _account) public view returns(Ticket[] memory) {
+        uint256 balanceOfAccount = ERC721.balanceOf(_account);
+        Ticket[] memory ticketsOfAccount = new Ticket[](balanceOfAccount);
+
+        // filtering the one that the account owned, because we didnt remove the sold tickets
+        // This is done instead of popping because it is less expensive
+        uint256 totalFound = 0;
+        uint totalPossibleTicketsOwnedNumber = accountToTickets[_account].length;
+        for (uint256 i = 0; i < totalPossibleTicketsOwnedNumber; i++) {
+            uint256 ticketId = accountToTickets[_account][i];
+            if (tickets[ticketId].owner == _account) {
+                ticketsOfAccount[totalFound] = tickets[ticketId];
+                totalFound += 1;
+                if (balanceOfAccount == totalFound) { // optimization
+                    return ticketsOfAccount;
+                }
+            }
+        }
+        return ticketsOfAccount;
+    }
+
+
 }
--- test/TicketNFT.test.js
+++ test/TicketNFT.test.js
@@ -22,6 +22,19 @@ contract("TicketNFT", accounts => {
         return await TicketNFT.at(ticketAddress.logs[0].args.ticket)
     }
 
+    /**
+     * Create a ticketNFT interface and mint all the tickets to the organizer
+     * @param initPrice
+     * @param totalTickets
+     * @param from
+     * @return {Promise<*>}
+     */
+    const createAndMintAll = async (initPrice, totalTickets, from) => {
+        const ticketInterface = await createTicketNFT(initPrice, totalTickets, from)
+        await ticketInterface.mint(organizer, totalTickets, { from: organizer })
+        return ticketInterface
+    }
+
     it("lets The organizer create a ticketNFT interface", async () => {
         const initPrice = 50
         const totalTickets = 30
@@ -48,5 +61,97 @@ contract("TicketNFT", accounts => {
 
         // no more tickets to mint, max reached
         await assert.rejects(ticketInterface.mint(organizer, totalTickets, { from: organizer }), /No more tickets available/)
+    })
+
+    it("Can get the tickets of an account", async () => {
+        const initPrice = 50
+        const totalTickets = 10
+        const ticketInterface = await createTicketNFT(initPrice, totalTickets, organizer)
+
+        let ticketsOfOrganizer = await ticketInterface.getTicketsOfAccount(organizer)
+        assert.strictEqual(ticketsOfOrganizer.length, 0, "Before minting there should be no ticket")
+
+        const ticketsToMint = 5
+        await ticketInterface.mint(organizer, ticketsToMint, { from: organizer })
+
+        ticketsOfOrganizer = await ticketInterface.getTicketsOfAccount(organizer)
+        assert.strictEqual(ticketsOfOrganizer.length, ticketsToMint, "There should be 5 tickets")
+        for (const ticket of ticketsOfOrganizer) {
+            assert.strictEqual(ticket.price, initPrice.toString())
+            assert.strictEqual(ticket.owner, organizer)
+        }
+    })
+
+    describe("Selling tickets", () => {
+        const initPrice = 100
+        const totalTickets = 10
+        let organizerTickets, ticketInterface
+
+        before(async () => {
+            ticketInterface = await createAndMintAll(initPrice, totalTickets, organizer)
+            organizerTickets = await ticketInterface.getTicketsOfAccount(organizer)
+        })
+
+        it("Organizer can sell one ticket to alice", async () => {
+            assert.strictEqual(organizerTickets.length, totalTickets, "Should have all the tickets")
+            // selling one ticket to alice
+            const ticketToSell = organizerTickets[0].id
+            await ticketInterface.sellTicket(organizer, alice, ticketToSell, initPrice, { from: organizer })
+            const ticket = await ticketInterface.tickets(ticketToSell)
+
+            assert.strictEqual(ticket.owner, alice)
+            organizerTickets = await ticketInterface.getTicketsOfAccount(organizer)
+            assert.strictEqual(organizerTickets.length, totalTickets - 1, "One should be sold to alice")
+
+            const aliceTickets = await ticketInterface.getTicketsOfAccount(alice)
+            assert.strictEqual(aliceTickets.length, 1)
+            assert.strictEqual(aliceTickets[0].id, ticketToSell)
+        })
+
+        it("Wont let alice sell a ticket to the organizer", async () => {
+            const [aliceTicket] = await ticketInterface.getTicketsOfAccount(alice)
+            assert(aliceTicket, "Should have one ticket")
+            await assert.rejects(ticketInterface.sellTicket(alice, organizer, aliceTicket.id, initPrice, { from: alice }),
+                /Can't sell to the organizer/)
+        })
+
+        it("wont let alice sell a ticket she doesn't own", async () => {
+            const [aliceTicket] = await ticketInterface.getTicketsOfAccount(alice)
+            assert(aliceTicket, "Should have one ticket")
+            const ticketToSell = organizerTickets[0]
+            assert(ticketToSell.id.toString() !== aliceTicket.id.toString())
+            await assert.rejects(ticketInterface.sellTicket(alice, bob, ticketToSell.id, initPrice, { from: alice }),
+                /ERC721: transfer of token that is not own/)
+        })
+
+        it("Wont let alice sell ticket to a higher price than 110%", async () => {
+            const [aliceTicket] = await ticketInterface.getTicketsOfAccount(alice)
+            assert(aliceTicket, "Should have one ticket")
+            const sellingPrice = Number(aliceTicket.price.toString())*2
+            await assert.rejects(ticketInterface.sellTicket(alice, bob, aliceTicket.id, sellingPrice, { from: alice }),
+                /Price is higher than 110% of previous sale/)
+        })
+
+        it("Wont let the organizer sell the ticket of alice", async () => {
+            const [aliceTicket] = await ticketInterface.getTicketsOfAccount(alice)
+            assert(aliceTicket, "Should have one ticket")
+
+            await assert.rejects(ticketInterface.sellTicket(alice, bob, aliceTicket.id, initPrice, { from: organizer }),
+                /Only owner of ticket can sell/)
+        })
+
+        it("Lets alice sell their ticket to bob", async () => {
+            const [aliceTicket] = await ticketInterface.getTicketsOfAccount(alice)
+            assert(aliceTicket, "Should have one ticket")
+            const sellingPrice = Math.floor(Number(aliceTicket.price.toString()) + Number(aliceTicket.price.toString())*8/100)
+            await ticketInterface.sellTicket(alice, bob, aliceTicket.id, sellingPrice, { from: alice })
+
+            const aliceTickets = await ticketInterface.getTicketsOfAccount(alice)
+            assert.strictEqual(aliceTickets.length, 0, "Alice shouldnt have any more tickets")
+            const bobTickets = await ticketInterface.getTicketsOfAccount(bob)
+            assert.strictEqual(bobTickets.length, 1, 'Should have the ticket')
+            assert.strictEqual(bobTickets[0].id, aliceTicket.id, 'The ticket is from alice')
+            assert.strictEqual(bobTickets[0].price, sellingPrice.toString())
+        })
     })
 });
